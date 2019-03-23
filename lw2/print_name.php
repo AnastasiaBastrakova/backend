e5e2befdb057b957665f7257bc766d10f415d622
AnastasiaBastrakova <nastyabastrakova@gmail.com>
2019-03-23 18:01:25 +0300

    lw2

---
+++ lw2/print_name.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/plain');
+echo 'Hello, ';
+if (isset($_GET['name']))
+    echo 'Dear '. $_GET['name'];
+else
+    echo 'unknown';
+echo '!';
---
+++ lw2/print_query_string.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type: text/plain');
+echo 'Query string = ';
+echo "'", $_SERVER["QUERY_STRING"], "'";
